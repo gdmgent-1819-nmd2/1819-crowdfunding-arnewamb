9e2bf3a06f2d60ff6b310b91342866089a42df8e
gdm-1718-arnewamb <arne.wambeke@student.arteveldehs.be>
2018-11-23 17:52:23 +0100

    updating bootstrap

---
+++ assets/scripts/stick-min.js
@@ -0,0 +1,1 @@
+var stickWidth=991,win=$(window),menu=$("#menu"),options={bottoming:!1,offset_top:0};win.width()>stickWidth?($(".stick").stick_in_parent(),console.log("stick")):($(".stick").trigger("sticky_kit:detach"),console.log("unstick"));
