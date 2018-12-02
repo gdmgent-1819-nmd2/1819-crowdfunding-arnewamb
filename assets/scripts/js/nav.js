67cb4163624665f585e8c2baa0e91b276920c657
arnewamb <arne.wambeke@student.arteveldehs.be>
2018-12-02 18:55:14 +0100

    fixing an navigation issue

--- assets/scripts/js/nav.js
+++ assets/scripts/js/nav.js
@@ -1,6 +1,7 @@
 /** Gestion du burger **/
 //$( "nav" ).hide();
 
+
 var tl = new  TimelineMax ();
 tl.staggerTo('.nav li', 0.3, {'opacity': 1, 'marginRight': 0}, 0.1);
 tl.pause();
@@ -8,9 +9,11 @@ tl.pause();
 $('.burger').click(function() {
   if(!$('.burger').hasClass('cross')){
     $('.burger').addClass('cross');
+    // $('#nav-main').removeClass('d-none');
     tl.play();
   } else {
     $('.burger').removeClass('cross');
+    // $('#nav-main').addClass('d-none');
     tl.reverse();
   }
 });
